af92fca8f4d3bc79744bac21ea47bd3ee28d3618
Ferhat Aslan <aslanferhat16@gmail.com>
2024-01-13 00:36:33 +0300

    cmpltd

---
+++ src/data/categories.ts
@@ -0,0 +1,11 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'Sewing Machines',
+	"Fabrics",
+	"Models",
+	"Knitting Machines",
+	"Sewing Tips",
+	"Home Textile",
+	"Manufacturers",
+	"Sewing Patterns"
+] as const
--- tina/config.ts
+++ tina/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "tinacms";
+import { CATEGORIES } from '../src/data/categories.ts'
 
 // Your hosting provider likely exposes this as an environment variable
 
@@ -12,38 +13,99 @@ export default defineConfig({
   token: "00b08c53736f23dbf2a2cc170dc8c62b02d9a9a9",
 
   build: {
-    outputFolder: "admin",
-    publicFolder: "public",
-  },
-  media: {
-    tina: {
-      mediaRoot: "",
-      publicFolder: "public",
-    },
-  },
-  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
-  schema: {
-    collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-    ],
-  },
+		outputFolder: 'admin',
+		publicFolder: 'public'
+	},
+	media: {
+		tina: {
+			mediaRoot: '/src/assets/images',
+			publicFolder: ''
+		}
+	},
+	schema: {
+		collections: [
+			{
+				name: 'post',
+				label: 'Blog Post',
+				path: 'src/content/blog',
+				format: 'mdx',
+				fields: [
+					{
+						type: 'image',
+						label: 'Cover Image',
+						required: true,
+						name: 'heroImage',
+						description: 'The image used for the cover of the post'
+					},
+
+					{
+						type: 'string',
+						required: true,
+						name: 'category',
+						label: 'Category',
+						description: 'Select an category for this post',
+						options: [...CATEGORIES]
+					},
+					{
+						type: 'string',
+						label: 'description',
+						required: true,
+						name: 'description',
+						description: 'A short description of the post'
+					},
+					{
+						type: 'datetime',
+						name: 'pubDate',
+						label: 'Publication Date',
+						required: true
+					},
+					{
+						name: 'draft',
+						label: 'Draft',
+						type: 'boolean',
+						description: 'If this is checked the post will not be published'
+					},
+					{
+						type: 'string',
+						name: 'tags',
+						required: true,
+						label: 'Tags',
+						description: 'Tags for this post',
+						list: true,
+						ui: {
+							component: 'tags'
+						}
+					},
+					{
+						type: 'string',
+						name: 'title',
+						label: 'Title',
+						isTitle: true,
+						required: true
+					},
+					{
+						type: 'rich-text',
+						label: 'Body',
+						name: 'SButton',
+						isBody: true,
+						templates: [
+							// Custom Components
+							{
+								label: 'SButton',
+								name: 'SButton',
+								fields: [
+									{
+										type: 'rich-text',
+										label: 'SButton',
+										name: 'children',
+										isBody: true
+									}
+								]
+							}
+						]
+					}
+				]
+			}
+		]
+	}
 });
